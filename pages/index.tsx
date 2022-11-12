a56ec909c0a47f8b469b56a531d04b013e502c74
rtseuztz <yoyoguy111@gmail.com>
2022-11-11 20:04:43 -0600

    removed unsued import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Hello from './hello'
 import { useEffect, useState } from 'react'
 
 export default function Home() {
