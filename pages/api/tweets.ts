c512896c415cb99a02c73681b380de4d36871188
rtseuztz <yoyoguy111@gmail.com>
2022-11-12 14:46:16 -0600

    fixed bug with retrieving

--- pages/api/tweets.ts
+++ pages/api/tweets.ts
@@ -10,18 +10,18 @@ export default async function handler(
     res: NextApiResponse<[Data] | null>
 ) {
     const method = req.method;
-    const body: Data = JSON.parse(req.body);
     console.log(JSON.parse(req.body).user);
     switch (method) {
         case 'GET':
             const rows = await query("SELECT * FROM tweets");
             return rows;
         case 'POST':
+            const body: Data = JSON.parse(req.body);
             await query("INSERT INTO tweets (user, tweet) VALUES ('" + body.user + "', '" + body.tweet + "')");
             return res.status(200).json([{ user: body.user, tweet: body.tweet }]);
-        case 'DELETE':
-            await query("DELETE FROM tweets WHERE id = " + req.body.id);
-            return res.status(200).json([{ user: body.user, tweet: body.tweet }]);
+        // case 'DELETE':
+        //     await query("DELETE FROM tweets WHERE id = " + req.body.id);
+        //     return res.status(200).json([{ user: body.user, tweet: body.tweet }]);
         default:
             return null;
     }
