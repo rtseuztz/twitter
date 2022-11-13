a5f750ffcabdd2d0d4e3092a36c5ef4bc72041a3
rtseuztz <yoyoguy111@gmail.com>
2022-11-13 12:25:56 -0600

    fixed backend point

--- pages/api/tweets.ts
+++ pages/api/tweets.ts
@@ -22,6 +22,11 @@ export default async function handler(
             await query("INSERT INTO tweets (user, tweet) VALUES ('" + body.user + "', '" + body.tweet + "')");
             res.status(200).json([{ user: body.user, tweet: body.tweet }]);
             return;
+        case 'DELETE':
+            const del: tweet = JSON.parse(req.body);
+            await query("DELETE FROM tweets WHERE user = '" + del.user + "' AND tweet = '" + del.tweet + "'");
+            res.status(200).json([{ user: del.user, tweet: del.tweet }]);
+            return;
         // case 'DELETE':
         //     await query("DELETE FROM tweets WHERE id = " + req.body.id);
         //     return res.status(200).json([{ user: body.user, tweet: body.tweet }]);
